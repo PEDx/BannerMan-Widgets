9d2c45f3f2ef09fd2484ec8e37dc44596927097d
pengdong <pengdong5566@163.com>
2019-07-25 12:07:16 +0800

    [feat] rollup 脚本

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # BannerMan-Widgets
 
-### lernajs
+### lernajs 命令
 
 - lerna init: 初始化项目
 - lerna bootstrap: 自动构建项目
@@ -8,3 +8,11 @@
 - lerna clean: 清理 node_modules 文件夹
 - lerna add: 添加依赖（类似 npm install)
 - lerna publish: 发版
+
+### 组件开发流程
+
+1. 在 packages 新建组件文件夹
+2. 使用 `npm init` 初始化 package.json, 使之成为可单独发布的 npm 包
+3. 新建 stories 文件夹, 添加 index.js 文件, 可使用 storybook 开发和调试
+4. 在项目根目录运行 `npm run build` 会构建所有组件
+5. `lerna publish` 会自动交互式添加版本号, 并运行 git 提交和 npm 发包操作,
--- scripts/rollup.config.base.js
+++ scripts/rollup.config.base.js
@@ -3,35 +3,31 @@ import commonjs from 'rollup-plugin-commonjs'; // 将CommonJS模块转换为 ES2
 import filesize from 'rollup-plugin-filesize';
 import vue from 'rollup-plugin-vue'; // 处理vue文件
 import babel from 'rollup-plugin-babel'; // rollup 的 babel 插件，ES6转ES5
-import css from 'rollup-plugin-css-only'; // 提取css，压缩能力不行
-import CleanCSS from 'clean-css'; // 压缩css
-import { writeFileSync } from 'fs'; // 写文件
+import postcss from 'rollup-plugin-postcss';
+import uglify from 'rollup-plugin-uglify-es';
+const path = require('path');
 
-const outputTypes = [{ file: './dist/es/index.js', format: 'es' }];
+const outputTypes = [{ file: './dist/index.js', format: 'es' }];
 
 const tasks = outputTypes.map(output => ({
-  input: './src/index.js',
-  external,
-  filesize,
+  input: './index.js',
   output,
   plugins: [
-    resolve(),
+    resolve({
+      extensions: ['.js', '.vue', '.json'],
+      dedupe: ['vue'],
+    }),
     filesize(),
     commonjs(),
-    css({
-      output(style) {
-        writeFileSync(
-          'dist/vue-rollup-component-template.css',
-          new CleanCSS().minify(style).styles,
-        );
-      },
+    postcss({
+      extract: true, // 提取 css 为单文件
+      minimize: true,
     }),
     vue({ css: false }),
     babel({
       exclude: 'node_modules/**',
-      plugins: ['external-helpers'],
     }),
-    svg(),
+    process.env.NODE_ENV === 'production' ? uglify() : null,
   ],
 }));
 

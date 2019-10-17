87d726723f423b0e3c4a8c5a849c0956f879c1f3
pengdong <pengdong5566@163.com>
2019-10-17 11:49:06 +0800

    feat: test

--- packages/widget-button/index.js
+++ packages/widget-button/index.js
@@ -4,6 +4,7 @@ import VTap from '../../common/v-tap';
 import profile from './profile';
 
 Vue.directive('tap', VTap);
+
 component._profile_ = profile;
 
 export default Vue.component('widget-button', component);

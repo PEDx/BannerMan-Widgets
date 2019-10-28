8fbd324976135dd426d7d7688372a80d0eb3ea97
pengdong <pengdong5566@163.com>
2019-10-28 16:28:21 +0800

    feat: 打包调试

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,10 +1,12 @@
 import resolve from 'rollup-plugin-node-resolve'; // 告诉 Rollup 如何查找外部模块
 import commonjs from 'rollup-plugin-commonjs'; // 将CommonJS模块转换为 ES2015 供 Rollup 处理
 import filesize from 'rollup-plugin-filesize';
+import fs from 'fs';
 import vue from 'rollup-plugin-vue'; // 处理vue文件
 import babel from 'rollup-plugin-babel'; // rollup 的 babel 插件，ES6转ES5
 import postcss from 'rollup-plugin-postcss';
 import replace from 'rollup-plugin-replace';
+var rimraf = require('rimraf');
 const { terser } = require('rollup-plugin-terser');
 const path = require('path');
 
@@ -13,10 +15,12 @@ const outputTypes = [
     name: 'index',
     file: './dist/index.js',
     format: 'umd',
-    intro: '// BannerMan',
   },
 ];
 const env = process.env.NODE_ENV;
+
+// 删除 dist 目录
+rimraf.sync('dist');
 
 const tasks = outputTypes.map(output => ({
   input: './index.js',
@@ -29,7 +33,7 @@ const tasks = outputTypes.map(output => ({
     filesize(), // 统计文件大小
     commonjs(), // 将 CommonJS 模块转换为 ES2015 供 Rollup 处理
     postcss({
-      extract: true, // 提取 css 为单文件
+      extract: false, // 提取 css 为单文件
       minimize: true,
     }), // 处理 css
     vue({ css: false }), // 处理vue文件
